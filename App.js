ef696a99361db394d4954b9c1340a35e6a0f248d
michaeltheo <45629601+michaeltheo@users.noreply.github.com>
2023-02-15 16:21:31 +0200

    Add - header

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import Header from "./src/components/Header";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text> lorem jwre ng</Text>
+      <Header />
+      <Text> lorem ng! wef</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+  // container: {
+  //   flex: 1,
+  //   backgroundColor: "#fff",
+  //   alignItems: "center",
+  //   justifyContent: "center",
+  // },
 });
